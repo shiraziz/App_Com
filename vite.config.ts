4006548477f0e4272d8681df0210dad7c92e6fd5
SHIRAZ <shiraz@MBP-de-Shiraz.home>
2025-01-22 22:41:14 +0100

    Application Gestion de Commande

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    open: true, // Ouvre automatiquement le navigateur
+  },
+  base: './', 
+});
